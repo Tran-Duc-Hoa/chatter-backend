ca7cf7f2b77c0da3bc16608b3c7c29a61c95dc76
Tran-Duc-Hoa <tranduchoa611@gmail.com>
2025-09-20 13:38:00 +0700

    Add health check endpoint

--- src/common/pubsub/pubsub.module.ts
+++ src/common/pubsub/pubsub.module.ts
@@ -1,34 +1,36 @@
-import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { RedisPubSub } from 'graphql-redis-subscriptions';
-import { PubSub } from 'graphql-subscriptions';
-import { Redis } from 'ioredis';
-import { PUB_SUB } from '../constants/injection-tokens';
-import { reviver } from './reviver';
+import { Module } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { RedisPubSub } from "graphql-redis-subscriptions";
+import { PubSub } from "graphql-subscriptions";
+import { Redis } from "ioredis";
+import { PUB_SUB } from "../constants/injection-tokens";
+import { reviver } from "./reviver";
 
 @Module({
   providers: [
     {
       provide: PUB_SUB,
       useFactory: (configService: ConfigService) => {
-        if (configService.get('NODE_ENV') === 'production') {
+        if (configService.get("NODE_ENV") === "production") {
           const options = {
-            host: configService.getOrThrow('REDIS_HOST'),
-            port: configService.getOrThrow('REDIS_PORT')
+            username: "default",
+            password: configService.getOrThrow("REDIS_PASSWORD"),
+            host: configService.getOrThrow("REDIS_HOST"),
+            port: configService.getOrThrow("REDIS_PORT"),
           };
 
           return new RedisPubSub({
             publisher: new Redis(options),
             subscriber: new Redis(options),
-            reviver
+            reviver,
           });
         }
 
         return new PubSub();
       },
-      inject: [ConfigService]
-    }
+      inject: [ConfigService],
+    },
   ],
-  exports: [PUB_SUB]
+  exports: [PUB_SUB],
 })
 export class PubSubModule {}
